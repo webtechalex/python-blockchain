30288dc07d81efd9cf9df93f24dbb1b888ed1319
Alex Brown <alexanderbrown.info@gmail.com>
2018-11-11 17:24:40 +0000

    complete mine() and new_transaction() methods on server

--- blockchain.py
+++ blockchain.py
@@ -66,12 +66,42 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return 'We will mine a new block...'
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+
+    return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return 'We will add a new transaction'
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    index = blockchain.new_transaction(values['ender'], values['recipient'], values['amount'])
+
+    response = {'message': 'Transaction will be added to block' + index}
+    return jsonify(response), 201
 
 
 @app.route('/chain', methods=['GET'])
