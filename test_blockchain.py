d74f560b6132b277950b0b6878d479fcd8c08f9f
Alex Brown <alexanderbrown.info@gmail.com>
2018-11-11 16:16:33 +0000

    implement basic methods

--- test_blockchain.py
+++ test_blockchain.py
@@ -1,9 +1,42 @@
 import unittest
+from time import time
 from blockchain import Blockchain
 
 
 class TestBlockchain(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.bc = Blockchain()
+
+    def test_chain(self):
+        self.assertIsInstance(self.bc.chain, list)
+
+    def test_current_transactions(self):
+        self.assertIsInstance(self.bc.current_transactions, list)
+
+    def test_new_block(self):
+        self.assertIsInstance(self.bc.new_block(proof=200), dict)
+        self.assertEqual(len(self.bc.chain), 2)
+
+    def test_hash(self):
+        block = {
+            'index': 1,
+            'timestamp': time(),
+            'transactions': [],
+            'proof': 123,
+            'previous_hash': None
+        }
+
+        self.assertIsInstance(Blockchain.hash(block), str)
+
+    def test_last_block(self):
+        self.bc.new_block(123)
+        self.assertIsInstance(self.bc.last_block, dict)
+
+    def test_new_transaction(self):
+        new_transaction_return = self.bc.new_transaction('alex', 'daniel', 1000)
+        expected = self.bc.last_block['index'] + 1
+        self.assertEqual(new_transaction_return, expected)
+        self.assertEqual(len(self.bc.current_transactions), 1)
 
 
 def test_suite():
@@ -14,6 +47,6 @@ def test_suite():
 BlockChainTestSuite = test_suite()
 
 runner = unittest.TextTestRunner()
-runner.run(TestBlockchain)
-# if __name__ == '__main__':
-#   unittest.main()
+runner.run(BlockChainTestSuite)
+if __name__ == '__main__':
+  unittest.main()
